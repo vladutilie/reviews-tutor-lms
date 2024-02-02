4497ecc228f219826599d58946e3c181af54639c
Vladut Ilie <vladilie94@gmail.com>
2024-02-02 16:36:27 +0200

    chore: main plugin file updated

--- tutor-reviews.php
+++ tutor-reviews.php
@@ -1,17 +1,19 @@
 <?php
 /**
- * Plugin Name:     Tutor Reviews
- * Description:     Add "Reviews" subbmenu in your Tutor LMS installation.
- * Author:          Vlăduț Ilie
- * Author URI:      https://vladilie.ro
- * Text Domain:     tutor-reviews
- * Domain Path:     /languages
- * Version:         0.1.0
+ * Plugin Name:       Tutor LMS Reviews
+ * Description:       Add "Reviews" submenu in your Tutor LMS installation.
+ * Author:            Vlăduț Ilie
+ * Author URI:        https://vladilie.ro
+ * Text Domain:       tutor-reviews
+ * Domain Path:       /languages
+ * Version:           0.1.0
+ * Requires at least: 4.6
+ * Requires PHP:      7.4
  *
- * @package         Tutor_Reviews
+ * @package           Tutor_Reviews
  */
 
-use TutorReviews\Includes;
+use TutorReviews\Includes\Main;
 
 defined( 'ABSPATH' ) || exit;
 
@@ -20,6 +22,6 @@ $plugin_root = plugin_dir_path( __FILE__ );
 /**
  * Require the main class of the plugin.
  */
-require_once $plugin_root . '/includes/class-reviews.php';
+require_once $plugin_root . '/includes/class-main.php';
 
-new Main();
+new Main( $plugin_root );
