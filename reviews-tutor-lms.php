b2139b16dd231de97ef431e19575aeaf040dc9cd
Vladut Ilie <vladilie94@gmail.com>
2024-03-20 08:51:43 +0200

    chore: plugin version incremented

--- reviews-tutor-lms.php
+++ reviews-tutor-lms.php
@@ -1,15 +1,15 @@
 <?php
 /**
  * Plugin Name:       Reviews for Tutor LMS
- * Description:       Add "Reviews" submenu in your Tutor LMS installation.
+ * Description:       This plugin enables the course reviews for Tutor LMS installation and allows you to manage them.
  * Author:            Vlad Ilie
  * Author URI:        https://vladilie.ro
  * License:           GPLv2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       reviews-tutor-lms
  * Domain Path:       /languages
- * Version:           1.0.0
- * Requires at least: 4.6
+ * Version:           1.0.1
+ * Requires at least: 6.4
  * Requires PHP:      7.4
  *
  * @package           Reviews_Tutor_LMS
